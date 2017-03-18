6a2ed6e5131b19c97351133810832367b4dc55d9
Twine <twineincorporated@gmail.com>
2017-03-18 15:48:14 -0400

    Add POST /todos route and refractor mongoose

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,19 +9,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
       }
       console.log('Connected to MongoDB server');
 
-      // deleteMany
-      db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((res) => {
-            console.log(res);
-      });
+      // // deleteMany
+      // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((res) => {
+      //       console.log(res);
+      // });
+      //
+      // // deleteOne
+      // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((res) => {
+      //       console.log(res);
+      // });
+      //
+      // // findOneAndDelete
+      // db.collection('Todos').findOneAndDelete({completed: false}).then(res) => {
+      //       console.log(res);
+      // });
 
-      // deleteOne
-      db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((res) => {
-            console.log(res);
-      });
+      db.collection('Users').deleteMany({name: 'Anthony'});
 
-      // findOneAndDelete
-      db.collection('Todos').findOneAndDelete({completed: false}).then(res) => {
-            console.log(res);
+      db.collection('Todos').findOneAndDelete({_id: new ObjectID("58ccc0c19446a30962018807")}).then((res) => {
+            console.log(JSON.stringify(res,undefined, 2));
       });
 
       //db.close();
